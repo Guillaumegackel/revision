8ad1d92724ea389a40f3f67886a9f4835e19db9c
Webme <guillaume.gackel@gmail.com>
2022-02-17 18:09:04 +0100

    base

---
+++ revisions.js
@@ -0,0 +1,19 @@
+const fruits = ['pomme', 'cerise', 'banane', 'cerise'];
+
+
+for (const i = 0; i < fruits.length, i++)
+console.log(i);
+
+for Of
+for (fruit of fruits)
+
+while
+
+// for each (peut etre reutilisé en rappelant la variable dans une fonction)
+
+find 
+
+filter 
+
+map le premier 
+
